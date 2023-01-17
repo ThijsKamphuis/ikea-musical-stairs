3b27a02ddca418b02465b309ddd81a7e46d27833
Thijs Kamphuis <1035120@hr.nl>
2023-01-17 01:43:23 +0100

    working build

--- main/main.ino
+++ main/main.ino
@@ -23,15 +23,29 @@ NewPing sonar[SONAR_NUM] = {
   NewPing(0, 0, TRAPCM),
   NewPing(1, 1, TRAPCM),
   NewPing(2, 2, TRAPCM),
-  NewPing(3, 3, TRAPCM), 
-  NewPing(4, 4, TRAPCM),
-  NewPing(5, 5, TRAPCM),
+  NewPing(5, 5, TRAPCM), 
+  NewPing(8, 8, TRAPCM),
+  NewPing(9, 9, TRAPCM),
   NewPing(10, 10, TRAPCM),
   NewPing(11, 11, TRAPCM),
   NewPing(12, 12, TRAPCM),
   NewPing(13, 13, TRAPCM)
 };
 
+int active[10] = {0,0,0,0,0,0,0,0,0,0};
+
+const char *soundFiles[] = {
+  "1.mp3",
+  "2.mp3",
+  "3.mp3",
+  "4.mp3",
+  "5.mp3",
+  "6.mp3",
+  "7.mp3",
+  "8.mp3",
+  "9.mp3",
+  "10.mp3",
+};
 
 void setup() {
   Serial.begin(115200);
@@ -43,51 +57,32 @@ void setup() {
   musicPlayer.setVolume(VOLUME,VOLUME);
   musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT);
 
-  printDirectory(SD.open("/"), 0);
-
 }
 
 
 
 void loop () {
     for (uint8_t i = 0; i < SONAR_NUM; i++) {
-    delay(20);
 
     int dist = sonar[i].ping_cm();
     if (dist != 0) {
-      musicPlayer.playFullFile("/1.mp3"); 
+      if (active[i] == 0) {
+        musicPlayer.startPlayingFile(soundFiles[i]);
+        active[i] = 1;
+      }
+    }// if dist
+    else {
+      active[i] = 0;
     }
+
     Serial.print(i);
     Serial.print("=");
     Serial.print(dist);
     Serial.print("cm ");
-  }
+
+    delay(30);
+  }// for
   Serial.println();
-}
+}// loop
 
 
-
-void printDirectory(File dir, int numTabs) {
-   while(true) {
-     
-     File entry =  dir.openNextFile();
-     if (! entry) {
-       // no more files
-       //Serial.println("**nomorefiles**");
-       break;
-     }
-     for (uint8_t i=0; i<numTabs; i++) {
-       Serial.print('\t');
-     }
-     Serial.print(entry.name());
-     if (entry.isDirectory()) {
-       Serial.println("/");
-       printDirectory(entry, numTabs+1);
-     } else {
-       // files have sizes, directories do not
-       Serial.print("\t\t");
-       Serial.println(entry.size(), DEC);
-     }
-     entry.close();
-   }
-}
